260b6db582aab4d74aef134c48aea00d6bd02e24
saikumar <sai680513@gmail.com>
2023-05-21 07:32:58 +0530

    create rendering functionality

---
+++ practice.py
@@ -0,0 +1,2 @@
+from example_pypi.youtube import render_YouTube_Video
+
---
+++ src/example_pypi/custom_exception.py
@@ -0,0 +1,5 @@
+class InvalidURLException(Exception):
+    def __init__(self , message: str = "URL is not valid"):
+        super().__init__(self.message)
+        
+
